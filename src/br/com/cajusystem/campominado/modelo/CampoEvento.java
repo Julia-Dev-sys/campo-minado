a3baddff053785d52eb033caaabd7de1ea8dedad
Julia Rocha <juliarralvim@hotmail.com>
2023-03-08 17:50:03 -0300

    first commit

---
+++ src/br/com/cajusystem/campominado/modelo/CampoEvento.java
@@ -0,0 +1,7 @@
+package br.com.cajusystem.campominado.modelo;
+
+public enum CampoEvento {
+	
+	ABRIR,MARCAR,DESMARCAR,EXPLODIR,REINICIAR;
+
+}
---
+++ src/br/com/cajusystem/campominado/modelo/CampoObservador.java
@@ -0,0 +1,8 @@
+package br.com.cajusystem.campominado.modelo;
+
+public interface CampoObservador {
+	
+	
+	public void eventoOcorreu(Campo campo,CampoEvento evento);
+
+}
